a6ff5fa63ebea0e964ada14a48b798aabeaf9808
Charlotte Webster <charlotte.shae.webster@gmail.com>
2022-11-09 15:57:16 -0700

    implement makeBoard

--- connect4.js
+++ connect4.js
@@ -7,11 +7,11 @@
  * board fills (tie)
  */
 
-var WIDTH = 7;
-var HEIGHT = 6;
+const WIDTH = 7;
+const HEIGHT = 6;
 
-var currPlayer = 1; // active player: 1 or 2
-var board = []; // array of rows, each row is array of cells  (board[y][x])
+let currPlayer = 1; // active player: 1 or 2
+let board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
@@ -19,6 +19,12 @@ var board = []; // array of rows, each row is array of cells  (board[y][x])
 
 function makeBoard() {
   // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+  const newRow = Array.from({ length: WIDTH }).fill(null);
+
+  for (let i = 0; i < HEIGHT; i++) {
+    board.push(newRow);
+  }
+
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
